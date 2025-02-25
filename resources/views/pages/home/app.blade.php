16d54c4591da6e673ac4678a23416cd4c3984ef3
Kraprow <kamalat.g@nsru.ac.th>
2025-02-25 15:08:08 +0700

    updatye

--- resources/views/pages/home/app.blade.php
+++ resources/views/pages/home/app.blade.php
@@ -4,4 +4,6 @@
     @include('pages.home.components.section-1')
     @include('pages.home.components.section-2')
     @include('pages.home.components.section-3')
+    @include('pages.home.components.section-4')
+    @include('pages.home.components.section-5')
 @endsection

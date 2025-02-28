816852386ba449ea5b3377ce2eeb1018524e7850
Kraprow <kamalat.g@nsru.ac.th>
2025-02-28 15:35:07 +0700

    update

--- resources/views/pages/home/app.blade.php
+++ resources/views/pages/home/app.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('title', 'แปลงยาว')
 @section('content')
+    @include('pages.home.components.video-top')
     @include('pages.home.components.section-1')
     @include('pages.home.components.section-2')
     @include('pages.home.components.section-3')
@@ -10,5 +11,6 @@
     @include('pages.home.components.section-7')
     @include('pages.home.components.section-8')
     @include('pages.home.components.section-9')
+    @include('pages.home.components.video-bottom')
     @include('pages.home.components.section-10')
 @endsection
---
+++ resources/views/pages/home/components/video-bottom.blade.php
@@ -0,0 +1,4 @@
+<video width="100%" autoplay loop muted playsinline style="object-fit: cover; margin:0px; padding:0px; display: block;">
+    <source src="{{ asset('images/video/ล่าง_1.mp4') }}" type="video/mp4">
+    Your browser does not support the video tag.
+</video>

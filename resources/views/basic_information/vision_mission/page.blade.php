c40ed063d73f7b3345d9d253a785a7a6d256aec1
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-03-11 09:30:00 +0700

    update

--- resources/views/basic_information/vision_mission/page.blade.php
+++ resources/views/basic_information/vision_mission/page.blade.php
@@ -23,7 +23,7 @@
     <div class="bg py-5">
         <div class="container py-5 custom-gradient-shadow">
             <div class=" d-flex flex-column justify-content-center p-5">
-                <div class="fs-1 fw-bold mb-4 text-center" style="color: #77b329;">วิสัยทัศน์/พันธกิจ</div>
+                <div class="fs-1 fw-bold mb-4 text-center">วิสัยทัศน์/พันธกิจ</div>
                 @forelse ($basicInfoDetail as $detail)
 
                 <p>{!! $detail->details !!}</p>

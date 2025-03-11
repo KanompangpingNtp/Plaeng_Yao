6a18376ae5018c00b5939bc6f4254bf850fffe1f
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-03-11 09:23:40 +0700

    ีupdate

--- resources/views/basic_information/history/page.blade.php
+++ resources/views/basic_information/history/page.blade.php
@@ -1,70 +1,71 @@
 @extends('layouts.sub-layout')
 @section('content')
-    <style>
-        .bg {
-            background-image: url('{{ asset('images/section-3/bg-4.png') }}');
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-            min-height: 100vh;
-        }
+<style>
+    .bg {
+        background-image: url('{{ asset('images/section-3/bg-4.png') }}');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        min-height: 100vh;
+    }
 
-        .custom-gradient-shadow {
-            border-radius: 30px;
-            box-shadow: 0 4px 15px rgba(0, 0, 0, 0.3),
-                /* เงาพื้นฐาน */
-                0 0 50px -10px rgba(158, 255, 3, 0.8),
-                /* เงาสีฟ้าเข้ม */
-                0 0 50px -10px rgba(72, 255, 0, 0.8);
-            /* เงาสีฟ้าอ่อน */
-            background-color: #ffffff;
-        }
-    </style>
-    <div class="bg py-5">
-        <div class="container py-5 custom-gradient-shadow">
-            <div class=" d-flex flex-column justify-content-center p-5">
-                <div class="fs-1 fw-bold mb-4 text-center">ประวัติความเป็นมา</div>
-                @forelse ($basicInfoDetail as $detail)
+    .custom-gradient-shadow {
+        border-radius: 30px;
+        box-shadow: 0 4px 15px rgba(0, 0, 0, 0.3),
+            /* เงาพื้นฐาน */
+            0 0 50px -10px rgba(158, 255, 3, 0.8),
+            /* เงาสีฟ้าเข้ม */
+            0 0 50px -10px rgba(72, 255, 0, 0.8);
+        /* เงาสีฟ้าอ่อน */
+        background-color: #ffffff;
+    }
 
-                <p>{!! $detail->details !!}</p>
+</style>
+<div class="bg py-5">
+    <div class="container py-5 custom-gradient-shadow">
+        <div class=" d-flex flex-column justify-content-center p-5">
+            <div class="fs-1 fw-bold mb-4 text-center">ประวัติความเป็นมา</div>
+            @forelse ($basicInfoDetail as $detail)
 
-                {{-- แสดงไฟล์ PDF --}}
-                @if ($detail->pdf->count() > 0)
-                <h6>ไฟล์ PDF:</h6>
-                <ul>
-                    @foreach ($detail->pdf as $pdf)
-                    <li>
-                        <a href="{{ asset('storage/' . $pdf->pdf_file) }}" target="_blank">
-                            ดาวน์โหลด PDF
-                        </a>
-                    </li>
-                    @endforeach
-                </ul>
-                @else
-                <p></p>
-                @endif
+            <p>{!! $detail->details !!}</p>
 
-                {{-- แสดงรูปภาพ --}}
-                @if ($detail->images->count() > 0)
-                <div class="d-flex flex-wrap">
-                    @foreach ($detail->images as $image)
-                    <div class="col-md-3 p-2">
-                        <img src="{{ asset('storage/' . $image->images_file) }}" class="img-fluid rounded" alt="รูปภาพ">
-                    </div>
-                    @endforeach
+            {{-- แสดงไฟล์ PDF --}}
+            @if ($detail->pdf->count() > 0)
+            <h6>ไฟล์ PDF:</h6>
+            <ul>
+                @foreach ($detail->pdf as $pdf)
+                <li>
+                    <a href="{{ asset('storage/' . $pdf->pdf_file) }}" target="_blank">
+                        ดาวน์โหลด PDF
+                    </a>
+                </li>
+                @endforeach
+            </ul>
+            @else
+            <p></p>
+            @endif
+
+            {{-- แสดงรูปภาพ --}}
+            @if ($detail->images->count() > 0)
+            <div class="d-flex flex-wrap justify-content-center align-items-center">
+                @foreach ($detail->images as $image)
+                <div class="col-md-3 p-2 d-flex justify-content-center">
+                    <img src="{{ asset('storage/' . $image->images_file) }}" class="img-fluid rounded" alt="รูปภาพ">
                 </div>
-                @else
-                <p></p>
-                @endif
-
-                @empty
-                <p class="text-center"></p>
-                @endforelse
-
+                @endforeach
             </div>
+            @else
+            <p class="text-center"></p>
+            @endif
+
+
+            @empty
+            <p class="text-center"></p>
+            @endforelse
+
         </div>
     </div>
+</div>
 
 
 @endsection
-
